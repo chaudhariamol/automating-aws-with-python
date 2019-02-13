be167934b5920e7224805408be32aebc916b3333
abc <abc@example.com>
2019-02-13 19:01:17 +0000

    add example aautoscaling

---
+++ autoscala_example.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+as_client = session.client('autoscaling')
+as_client.describe_auto_scaling_group()
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+as_client = session.client('autoscaling')
+as_client.describe_auto_scaling_group
+as_client.describe_auto_scaling_group()
+as_client.describe_auto_scaling_groups()
+as_client.describe_policies()
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScaleDown')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScaleDown')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScaleDown')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScaleUp')
+as_client.describe_policies()
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScalaUp')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScalaUp')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScaleDown')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScalaUp')
+as_client.execute_policy(AutoScalingGroupName= 'NotifonExampleGroup', PolicyName= 'ScaleDown')
